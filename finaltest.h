9a7b079548952b914776964977bb135060663911
agnieszkakoltys <agnieszkakoltys@gmail.com>
2018-10-30 14:05:16 -0700

    Update finaltest.h

--- finaltest.h
+++ finaltest.h
@@ -1,4 +1,4 @@
-// finaltest.h
+ // finaltest.h
 #include <cxxtest/TestSuite.h>
 #include "test.h"
 
@@ -11,22 +11,30 @@ public:
     }
     void test2(void)
     {
-       TS_ASSERT_EQUALS(month_number, real_month_number);
+       TS_ASSERT_DELTA(language_name_lenght, 6, 0.0001)
     }
     void test3(void)
     {
-    TS_ASSERT_DIFFERS(month_number, real_month_number);
+    TS_ASSERT_DELTA(language_name_lenght, 9, 0.0001)
     }
     void test4(void)
     {
-    TS_ASSERT_LESS_THAN(month_number, real_month_number);
+    TS_ASSERT_LESS_THAN(language_name_lenght, 6)
     }
 
     void test5(void)
     {
-    TS_ASSERT_LESS_THAN_EQUALS(month_number, real_month_number);
+    TS_ASSERT_LESS_THAN(language_name_lenght, 9)
     }
-    void test6(void)
+   void test7(void)
+    {
+    TS_ASSERT_LESS_THAN(month_number, 1)
+    }
+   void test8(void)
+    {
+    TS_ASSERT_LESS_THAN(12, month_number)
+    }
+    void test9(void)
     {
          TS_WARN("TODO: Check invalid parameters");
     }
